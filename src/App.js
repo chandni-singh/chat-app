3839930ee9661384f6930f781fb5c5e953f51252
Chandni <chandni1141996@gmail.com>
2023-10-16 21:26:11 +0530

    cleaned code, added react chat engine to app

--- src/App.js
+++ src/App.js
@@ -1,23 +1,15 @@
-import logo from './logo.svg';
-import './App.css';
+import { ChatEngine } from "react-chat-engine";
+import "./App.css";
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <ChatEngine
+        height="100vh"
+        projectID="486b5bf3-523d-4893-8bd0-95778532db17"
+        userName="john"
+        userSecret="qwerty"
+      />
     </div>
   );
 }
