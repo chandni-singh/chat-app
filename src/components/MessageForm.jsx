b83c4db03f8b539f8436b1e4a9573782d0602c6c
Chandni <chandni1141996@gmail.com>
2023-10-17 21:44:49 +0530

    added code for message form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -1,5 +1,40 @@
-const MessageForm = () => {
-  return <div>ChatFeed</div>;
+import { useState } from "react";
+import { SendOutlined, PictureOutlined } from "@ant-design/icons";
+import { sendMessage, isTyping } from "react-chat-engine";
+
+const MessageForm = (props) => {
+  const { chatId, creds } = props;
+  const [value, setValue] = useState("");
+
+  return (
+    <form className="message-form" onSubmit={handleSubmit}>
+      <input
+        className="message-input"
+        placeholder="Send a message..."
+        value={value}
+        onChnage={handleChange}
+        onSubmit={handleSubmit}
+      />
+
+      <label htmlFor="upload-button">
+        <span className="image-button">
+          <PictureOutlined className="picture-icon" />
+        </span>
+      </label>
+
+      <input
+        type="file"
+        multiple={false}
+        id="upload-button"
+        style={{ display: "none" }}
+        onChange={handleUpload.bind(this)}
+      />
+
+      <button type="submit" className="send-button">
+        <SendOutlined className="send-icon" />
+      </button>
+    </form>
+  );
 };
 
 export default MessageForm;
