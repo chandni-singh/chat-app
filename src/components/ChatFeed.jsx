f0a0634dfb6548f97064da4006d5d6e361ffa838
Chandni <chandni1141996@gmail.com>
2023-10-17 21:27:18 +0530

    added code to render read receipts

--- src/components/ChatFeed.jsx
+++ src/components/ChatFeed.jsx
@@ -7,6 +7,22 @@ const ChatFeed = (props) => {
 
   const chat = chats && chats[activeChat];
 
+  const renderReadReceipts = (message, isMyMessage) =>
+    chat.people.map(
+      (person, index) =>
+        person.last_read === message.id && (
+          <div
+            key={`read_${index}`}
+            className="read-receipt"
+            style={{
+              float: isMyMessage ? "right" : "left",
+              backgroundImage:
+                person.person.avatar && `url(${person.person.avatar})`,
+            }}
+          ></div>
+        )
+    );
+
   const renderMessages = () => {
     const keys = Object.keys(messages);
 
@@ -34,7 +50,9 @@ const ChatFeed = (props) => {
               marginRight: isMyMessage ? "18px" : "0px",
               marginLeft: isMyMessage ? "0px" : "68px",
             }}
-          ></div>
+          >
+            {renderReadReceipts(message, isMyMessage)}
+          </div>
         </div>
       );
     });
