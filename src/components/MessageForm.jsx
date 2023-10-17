37508cf5f99088c1487d70c341e9d1ddf5f0b9c0
Chandni <chandni1141996@gmail.com>
2023-10-17 21:45:37 +0530

    added code for methods in message form

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -6,6 +6,26 @@ const MessageForm = (props) => {
   const { chatId, creds } = props;
   const [value, setValue] = useState("");
 
+  const handleChange = (event) => {
+    setValue(event.target.value);
+    isTyping(props, chatId);
+  };
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+
+    const text = value.trim();
+
+    if (text.length > 0) {
+      sendMessage(creds, chatId, { text });
+    }
+    setValue("");
+  };
+
+  const handleUpload = (event) => {
+    sendMessage(creds, chatId, { files: event.target.files, text: " " });
+  };
+
   return (
     <form className="message-form" onSubmit={handleSubmit}>
       <input
