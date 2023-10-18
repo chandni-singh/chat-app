2395ae28b1c2b6d4b0f0055d6811fa8bf4b5b4fe
Chandni <chandni1141996@gmail.com>
2023-10-18 21:57:06 +0530

    further fix

--- src/components/MessageForm.jsx
+++ src/components/MessageForm.jsx
@@ -23,7 +23,7 @@ const MessageForm = (props) => {
   };
 
   const handleUpload = (event) => {
-    sendMessage(creds, chatId, { files: event.target.files, text: " " });
+    sendMessage(creds, chatId, { files: event.target.files, text: "" });
   };
 
   return (
